d25fda09186b1adae65701fad3be04202652628f
Dipan Ghosh <dipan.ghosh@hotmail.com>
2021-01-03 22:48:32 +0530

    imported todolist

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <TodoList/>
   </div>
 </template>
+
+<script>
+import TodoList from '@/components/TodoList.vue'
+export default {
+  
+  components: {
+    TodoList
+  }
+}
+</script>
