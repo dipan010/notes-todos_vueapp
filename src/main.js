27674c4c143ab7647d0b8887307ebc978009c43c
Dipan Ghosh <dipan.ghosh@hotmail.com>
2021-01-03 22:53:30 +0530

    imported all assets

--- src/main.js
+++ src/main.js
@@ -2,5 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import './assets/css/styles.css'
+import './assets/css/createnotestyle.css'
+import './assets/css/todoListStyles.css'
 createApp(App).use(store).use(router).mount('#app')
