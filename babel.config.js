b5b821d0494ebcc3a2bb983d7e88a36adf7654b5
Dipan Ghosh <dipan.ghosh@hotmail.com>
2021-01-04 14:58:39 +0530

    hopefully

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ?
-        '/notes-todos_vueapp/' :
-        '/'
+    presets: [
+        '@vue/cli-plugin-babel/preset'
+    ]
 }
